94f5641b3453e6205371fd675ce3181335fc4430
cutajarj <james.cutajar@gmail.com>
2020-07-27 14:24:33 +0100

    Adding file search

--- joins/concurrent_file_search.py
+++ joins/concurrent_file_search.py
@@ -1,21 +1,32 @@
 import os
 from os.path import isdir, join
+from threading import Lock, Thread
 
+mutex = Lock()
 matches = []
 
 
 def file_search(root, filename):
     print("Searching in:", root)
+    threads = []
     for file in os.listdir(root):
         full_path = join(root, file)
         if filename in file:
+            mutex.acquire()
             matches.append(full_path)
+            mutex.release()
         if isdir(full_path):
-            file_search(full_path, filename)
+            t = Thread(target=file_search, args=([full_path, filename]))
+            t.start()
+            threads.append(t)
+    for t in threads:
+        t.join()
 
 
 def main():
-    file_search("c:/tools", "README.md")
+    t = Thread(target=file_search, args=(["c:/tools", "README.md"]))
+    t.start()
+    t.join()
     for m in matches:
         print("Matched:", m),
 
