c9c7a145c429e6f8c179f03b460cc991ab4ed379
cutajarj <james.cutajar@gmail.com>
2020-09-18 10:52:59 +0100

    Adding sync stingy and spendy in condition variables

---
+++ conditionVariables/stingy_spendy_cond_variable.py
@@ -0,0 +1,33 @@
+import time
+from threading import Thread, Condition
+
+
+class StingySpendy:
+    money = 100
+    cv = Condition()
+
+    def stingy(self):
+        for i in range(1000000):
+            self.cv.acquire()
+            self.money += 10
+            self.cv.notify()
+            self.cv.release()
+        print("Stingy Done")
+
+    def spendy(self):
+        for i in range(500000):
+            self.cv.acquire()
+            while (self.money < 20):
+                self.cv.wait()
+            self.money -= 20
+            if self.money < 0:
+                print("Money in bank", self.money)
+            self.cv.release()
+        print("Spendy Done")
+
+
+ss = StingySpendy()
+Thread(target=ss.stingy, args=()).start()
+Thread(target=ss.spendy, args=()).start()
+time.sleep(5)
+print("Money in the end", ss.money)
