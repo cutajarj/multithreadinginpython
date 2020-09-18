bb1eedc657a2138927eabf7fa1f7138837bbd877
cutajarj <james.cutajar@gmail.com>
2020-09-18 08:43:25 +0100

    Modifying concurrent file search to use wait group

---
+++ conditionVariables/concurrent_file_search_wg.py
@@ -0,0 +1,36 @@
+import os
+from os.path import isdir, join
+from threading import Lock, Thread
+
+from conditionVariables.wait_group import WaitGroup
+
+mutex = Lock()
+matches = []
+
+
+def file_search(root, filename, wait_group):
+    print("Searching in:", root)
+    for file in os.listdir(root):
+        full_path = join(root, file)
+        if filename in file:
+            mutex.acquire()
+            matches.append(full_path)
+            mutex.release()
+        if isdir(full_path):
+            t = Thread(target=file_search, args=([full_path, filename, wait_group]))
+            t.start()
+            wait_group.add(1)
+    wait_group.done()
+
+
+def main():
+    wait_group = WaitGroup()
+    wait_group.add(1)
+    t = Thread(target=file_search, args=(["c:/tools", "README.md", wait_group]))
+    t.start()
+    wait_group.wait()
+    for m in matches:
+        print("Matched:", m)
+
+
+main()
