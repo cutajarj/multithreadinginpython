be319b6b5c7919926c66cd9f81774729db9ef281
cutajarj <james.cutajar@gmail.com>
2020-10-24 08:49:25 +0100

    changing letter frequency to get txt files form the web instead of files

--- letter_frequencies/letter_counter.py
+++ letter_frequencies/letter_counter.py
@@ -1,26 +1,31 @@
-import os
 import json
+import urllib.request
 import time
+from threading import Thread
+
+finished_count = 0
 
 
-def count_letters(file, start, end, frequency):
-    f = open(file, "r", encoding="utf8")
-    f.seek(start)
-    txt = f.read(end - start)
+def count_letters(url, frequency):
+    response = urllib.request.urlopen(url)
+    txt = str(response.read())
     for l in txt:
         letter = l.lower()
         if letter in frequency:
             frequency[letter] += 1
+    global finished_count
+    finished_count += 1
 
 
 def main():
     frequency = {}
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
-    filename = "feedbacks.txt"
-    size = os.stat(filename).st_size
     start = time.time()
-    count_letters(filename, 0, size, frequency)
+    for i in range(1000,1020):
+        Thread(target=count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)).start()
+    while finished_count < 20:
+        time.sleep(0.5)
     end = time.time()
     print("Done, time taken", end - start)
     print(json.dumps(frequency, indent=4))
