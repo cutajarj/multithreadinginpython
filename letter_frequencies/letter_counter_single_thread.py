c57fb01d024b86029d62657c4086095a3dc62dad
cutajarj <james.cutajar@gmail.com>
2020-10-26 08:27:14 +0000

    Adding single threaded version

---
+++ letter_frequencies/letter_counter_single_thread.py
@@ -0,0 +1,28 @@
+import json
+import urllib.request
+
+import time
+
+
+def count_letters(url, frequency):
+    response = urllib.request.urlopen(url)
+    txt = str(response.read())
+    for l in txt:
+        letter = l.lower()
+        if letter in frequency:
+            frequency[letter] += 1
+
+
+def main():
+    frequency = {}
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        frequency[c] = 0
+    start = time.time()
+    for i in range(1000, 1020):
+        count_letters(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
+    end = time.time()
+    print("Done, time taken", end - start)
+    print(json.dumps(frequency, indent=4))
+
+
+main()
