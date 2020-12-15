8c19dfad2fd70f3777a5fb6696a02ead87bae832
cutajarj <james.cutajar@gmail.com>
2020-12-15 09:34:19 +0000

    Changing so it uses one acquire and one release in the function

--- letter_frequencies/letter_counter.py
+++ letter_frequencies/letter_counter.py
@@ -9,13 +9,11 @@ finished_count = 0
 def count_letters(url, frequency, mutex):
     response = urllib.request.urlopen(url)
     txt = str(response.read())
+    mutex.acquire()
     for l in txt:
         letter = l.lower()
-        mutex.acquire()
         if letter in frequency:
             frequency[letter] += 1
-        mutex.release()
-    mutex.acquire()
     global finished_count
     finished_count += 1
     mutex.release()
@@ -27,7 +25,7 @@ def main():
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
     start = time.time()
-    for i in range(1000,1020):
+    for i in range(1000, 1020):
         Thread(target=count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency, mutex)).start()
     while True:
         mutex.acquire()
@@ -36,8 +34,8 @@ def main():
         mutex.release()
         time.sleep(0.5)
     end = time.time()
-    print("Done, time taken", end - start)
     print(json.dumps(frequency, indent=4))
+    print("Done, time taken", end - start)
 
 
 main()
